23e47e60aadf695d3af3ecfd7dc5b3b5ad20d703
Radhe <theradhe.s@gmail.com>
2026-09-20 23:31:10 +0530

    host fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
   const user = await getCurrentUser();
 
   if (!user) {
-    redirect("/login");
+    redirect("api/auth/login");
   }
   return (
     <html
